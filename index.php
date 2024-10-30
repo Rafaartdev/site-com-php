c10e5621a7c95dd81912065188b02cb79a2a173d
Rafaartdev <silveira.orafa@gmail.com>
2024-10-30 14:25:07 -0300

    efetuando site-exemplo
    mais duas paginas

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site</title>
+</head>
+
+<body>
+    <header>
+        <h1>Site Exemplo</h1>
+        <nav>
+            <a href="">Home</a>
+            <a href="">Equipe</a>
+            <a href="">Serviços</a>
+        </nav>
+
+    </header>
+
+
+
+
+    <main>
+        <article>
+            <h2>Bem-vindo</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem dolorem tenetur veritatis ab reprehenderit. Dolor, exercitationem! Hic, eum ipsum? Tempore unde consectetur quis harum eveniet exercitationem dolorum esse laboriosam nam?</p>
+
+
+        </article>
+
+
+    </main>
+
+
+    <footer>
+        <h2>Site criado como exemplo</h2>
+    </footer>
+</body>
+
+</html>
---
+++ servicos.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>serviços - Site</title>
+</head>
+
+<body>
+    <header>
+        <h1>Site Exemplo</h1>
+        <nav>
+            <a href="">Home</a>
+            <a href="">Equipe</a>
+            <a href="">Serviços</a>
+        </nav>
+
+    </header>
+
+
+
+
+    <main>
+        <article>
+            <h2>Serviços</h2>
+            <ul>
+                <li>sites</li>
+                <li>concertos</li>
+                <li>transporte de peças</li>
+
+            </ul>
+
+        </article>
+
+
+    </main>
+
+
+    <footer>
+        <h2>Site criado como exemplo</h2>
+    </footer>
+</body>
+
+</html>
